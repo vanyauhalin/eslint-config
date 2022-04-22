e629b176ae9fbbdc5adf35db41e353e5cd222c2e
vanyauhalin <vanyauhalin@gmail.com>
2022-04-22 20:04:20 +0300

    feat(base): add main

---
+++ packages/eslint-config-base/index.ts
@@ -0,0 +1,64 @@
+import { addRules, override } from './src/utils';
+
+const root = override({
+  files: './*.{cjs,js}',
+  env: {
+    es2022: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: addRules([
+    'import/exports-last',
+    'import/group-exports',
+    'import/no-default-export',
+    'import/no-deprecated',
+    'import/no-extraneous-dependencies',
+    'import/order',
+    'import/prefer-default-export',
+    'max-len',
+    'sort-imports',
+  ]),
+});
+
+const src = override({
+  files: 'src/**/*.js',
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: addRules([
+    'import/exports-last',
+    'import/group-exports',
+    'import/no-commonjs',
+    'import/no-default-export',
+    'import/no-deprecated',
+    'import/order',
+    'import/prefer-default-export',
+    'import/unambiguous',
+    'max-len',
+    'sort-imports',
+  ]),
+});
+
+const test = override({
+  ...src,
+  files: 'test/*.js',
+});
+
+const overrides = [root, src, test];
+
+export {
+  overrides,
+};
