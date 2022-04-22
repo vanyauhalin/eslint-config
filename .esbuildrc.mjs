fc2a4d3e98dd8730c581159d0b1645728d8f89c8
vanyauhalin <vanyauhalin@gmail.com>
2022-04-22 20:38:02 +0300

    chore: add scripts for base

---
+++ .esbuildrc.mjs
@@ -0,0 +1,19 @@
+import { build } from 'esbuild';
+
+const [, , pack] = process.argv;
+const root = `./packages/eslint-config-${pack}`;
+
+if (!pack) {
+  throw new Error('Please specify a package to build');
+}
+
+build({
+  allowOverwrite: true,
+  bundle: true,
+  entryPoints: [root],
+  outfile: `${root}/lib/index.js`,
+  platform: 'node',
+}).catch((error) => {
+  process.stderr.write(error.stderr);
+  process.exit(1);
+});
