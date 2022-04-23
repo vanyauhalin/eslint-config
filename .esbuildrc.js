5fd1f504af2fc24ebb8f76d9c37e1e3c28541cf1
vanyauhalin <vanyauhalin@gmail.com>
2022-04-23 16:34:23 +0300

    chore: add heleper fn for build commands

--- .esbuildrc.js
+++ .esbuildrc.js
@@ -1,15 +1,29 @@
 import { build } from 'esbuild';
 
-const [, , name] = process.argv;
-const root = `./packages/eslint-config-${name}`;
+function getOptions() {
+  const [, , name] = process.argv;
+  const root = `./packages/eslint-config-${name}`;
+  const outfile = `${root}/lib/index.cjs`;
+  return {
+    name,
+    outfile,
+    root,
+  };
+}
+
+const { outfile, root } = getOptions();
 
 build({
+  outfile,
   allowOverwrite: true,
   bundle: true,
   entryPoints: [`${root}/src`],
-  outfile: `${root}/lib/index.cjs`,
   platform: 'node',
 }).catch((error) => {
   process.stderr.write(error.stderr);
   process.exit(1);
 });
+
+export {
+  getOptions,
+};
