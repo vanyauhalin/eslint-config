7f6c272f7ecec4968cd11e0804d86d85ddee784b
vanyauhalin <vanyauhalin@gmail.com>
2022-04-28 19:41:08 +0300

    test: change to uvu command

--- test/validate.js
+++ test/validate.js
@@ -1,13 +1,23 @@
-import { execSync } from 'child_process';
-import { getOptions } from '../.esbuildrc';
+import { ESLint } from 'eslint';
+import { test } from 'uvu';
+import { is } from 'uvu/assert';
+import { read } from '../.esbuildrc';
 
-const { outfile } = getOptions();
+test('eslint should throw an configs error', async () => {
+  await Promise.all(read().map(async (pack) => {
+    const eslint = new ESLint({
+      allowInlineConfig: false,
+      baseConfig: {
+        extends: pack.main,
+      },
+    });
+    const result = await eslint.lintFiles('./test/reference.js');
+    const hasError = result.every((el) => (
+      el.errorCount > 0
+      && el.fatalErrorCount === 0
+    ));
+    is(hasError, true);
+  }));
+});
 
-try {
-  execSync(`eslint -c ${outfile} --no-ignore ${outfile}`);
-  throw new Error('ESLint should have failed');
-} catch (error) {
-  if (!error.stdout) {
-    throw new Error('ESLint failed through no fault config');
-  }
-}
+test.run();
