176990c32372f22ddb0608de7697f224e3844e0d
vanyauhalin <vanyauhalin@gmail.com>
2022-05-14 01:51:31 +0300

    test: add more reference files

--- test/validate.js
+++ test/validate.js
@@ -1,23 +1,42 @@
+import { extname, resolve } from 'path';
 import { ESLint } from 'eslint';
 import { test } from 'uvu';
 import { is } from 'uvu/assert';
-import { read } from '../.esbuildrc';
 
-test('eslint should throw an configs error', async () => {
-  await Promise.all(read().map(async (pack) => {
-    const eslint = new ESLint({
-      allowInlineConfig: false,
-      baseConfig: {
-        extends: `${pack.library}/index.js`,
-      },
-    });
-    const result = await eslint.lintFiles('./test/reference.js');
-    const hasError = result.every((el) => (
-      el.errorCount > 0
-      && el.fatalErrorCount === 0
-    ));
-    is(hasError, true);
-  }));
+const PACKAGES = resolve('packages');
+const REFERENCE = resolve('test/reference');
+
+[
+  ['base', ['base.js']],
+  ['react', ['react.tsx']],
+  ['typescript', ['typescript.ts']],
+].forEach((options) => {
+  const [name, reference] = options;
+  const fullName = `eslint-config-${name}`;
+  test(`${fullName} should throw an configs error`, async () => {
+    await Promise.all(reference.map(async (file) => {
+      const extension = extname(file);
+      const eslint = new ESLint({
+        allowInlineConfig: false,
+        baseConfig: {
+          extends: `${PACKAGES}/${fullName}/lib/index.js`,
+          ...extension === '.ts' || extension === '.tsx'
+            ? {
+              parserOptions: {
+                project: `${REFERENCE}/tsconfig.json`,
+              },
+            }
+            : {},
+        },
+      });
+      const report = await eslint.lintFiles(`${REFERENCE}/${file}`);
+      const hasError = report.every((result) => (
+        result.errorCount > 0
+        && result.fatalErrorCount === 0
+      ));
+      is(hasError, true);
+    }));
+  });
 });
 
 test.run();
