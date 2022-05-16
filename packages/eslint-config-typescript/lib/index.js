e9e60a0b5161c14860162b1bb96357b06432305e
vanyauhalin <vanyauhalin@gmail.com>
2022-05-16 13:06:25 +0300

    fix(typescript): extends order

--- packages/eslint-config-typescript/lib/index.js
+++ packages/eslint-config-typescript/lib/index.js
@@ -1,3 +1,4 @@
+const base = require('@vanyauhalin/eslint-config-base');
 const utils = require('@vanyauhalin/eslint-config-base/lib/utils');
 const eslintPluginImport = require('./eslint-plugin-import');
 const typescriptEslint = require('./typescript-eslint');
@@ -8,20 +9,24 @@ const { add } = utils.group({
 });
 
 module.exports = utils.configure({
+  ...base,
   extends: [
+    ...base.extends,
     'airbnb-typescript/base',
-    '@vanyauhalin/eslint-config-base',
   ],
-  rules: add([
-    '@typescript-eslint/array-type',
-    '@typescript-eslint/consistent-type-imports',
-    '@typescript-eslint/explicit-function-return-type',
-    '@typescript-eslint/explicit-module-boundary-types',
-    '@typescript-eslint/lines-between-class-members',
-    '@typescript-eslint/member-delimiter-style',
-    '@typescript-eslint/member-ordering',
-    '@typescript-eslint/method-signature-style',
-    '@typescript-eslint/no-confusing-void-expression',
-    'import/no-extraneous-dependencies',
-  ]),
+  rules: {
+    ...base.rules,
+    ...add([
+      '@typescript-eslint/array-type',
+      '@typescript-eslint/consistent-type-imports',
+      '@typescript-eslint/explicit-function-return-type',
+      '@typescript-eslint/explicit-module-boundary-types',
+      '@typescript-eslint/lines-between-class-members',
+      '@typescript-eslint/member-delimiter-style',
+      '@typescript-eslint/member-ordering',
+      '@typescript-eslint/method-signature-style',
+      '@typescript-eslint/no-confusing-void-expression',
+      'import/no-extraneous-dependencies',
+    ]),
+  },
 });
