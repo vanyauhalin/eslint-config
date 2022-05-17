7eb0d8c825801d3ff2af17e91ea2738c66711a46
vanyauhalin <vanyauhalin@gmail.com>
2022-05-18 00:00:24 +0300

    fix(react): overrides

--- packages/eslint-config-react/lib/index.js
+++ packages/eslint-config-react/lib/index.js
@@ -1,16 +1,27 @@
+const typescript = require('@vanyauhalin/eslint-config-typescript');
+
 /**
  * @type {import('eslint').Linter.BaseConfig}
  */
 module.exports = {
   extends: [
-    '@vanyauhalin/eslint-config-typescript',
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
+    '@vanyauhalin/eslint-config-base/lib/eslint',
+    '@vanyauhalin/eslint-config-base/lib/eslint-plugin-import',
+    '@vanyauhalin/eslint-config-base/lib/eslint-plugin-jsonc',
+    '@vanyauhalin/eslint-config-base/lib/eslint-plugin-promise',
+    '@vanyauhalin/eslint-config-base/lib/eslint-plugin-unicorn',
+    '@vanyauhalin/eslint-config-typescript/lib/eslint-plugin-import',
+    '@vanyauhalin/eslint-config-typescript/lib/typescript-eslint',
     ...[
       './eslint-plugin-jsx-a11y',
       './eslint-plugin-react',
       './eslint-plugin-unicorn',
     ].map((config) => require.resolve(config)),
+  ],
+  overrides: [
+    ...typescript.overrides,
   ],
 };
