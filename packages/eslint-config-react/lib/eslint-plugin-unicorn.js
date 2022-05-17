911dd319f44a21e4d68313d48adfb0d60d8eba10
vanyauhalin <vanyauhalin@gmail.com>
2022-05-17 14:19:51 +0300

    feat(react): overwrite unicorn/filename-case rule

---
+++ packages/eslint-config-react/lib/eslint-plugin-unicorn.js
@@ -0,0 +1,12 @@
+module.exports = {
+  /**
+   * Enforce a case style for filenames.
+   * @see https://github.com/sindresorhus/eslint-plugin-unicorn/blob/47c8337f9fca67d91461e04d50d6f04583fd8652/docs/rules/filename-case.md
+   */
+  'unicorn/filename-case': ['error', {
+    cases: {
+      kebabCase: true,
+      pascalCase: true,
+    },
+  }],
+};
