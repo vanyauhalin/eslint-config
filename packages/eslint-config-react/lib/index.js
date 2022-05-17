afc0d3ec0434f9bf4f32a16d58c1f3f87b9132dc
vanyauhalin <vanyauhalin@gmail.com>
2022-05-17 23:24:25 +0300

    refactor(react): move typescript to extends

--- packages/eslint-config-react/lib/index.js
+++ packages/eslint-config-react/lib/index.js
@@ -1,4 +1,3 @@
-const typescript = require('@vanyauhalin/eslint-config-typescript');
 const eslintPluginJsxA11y = require('./eslint-plugin-jsx-a11y');
 const eslintPluginReact = require('./eslint-plugin-react');
 
@@ -6,15 +5,13 @@ const eslintPluginReact = require('./eslint-plugin-react');
  * @type {import('eslint').Linter.BaseConfig}
  */
 module.exports = {
-  ...typescript,
   extends: [
-    ...typescript.extends,
+    '@vanyauhalin/eslint-config-typescript',
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
   ],
   rules: {
-    ...typescript.rules,
     ...eslintPluginJsxA11y,
     ...eslintPluginReact,
   },
