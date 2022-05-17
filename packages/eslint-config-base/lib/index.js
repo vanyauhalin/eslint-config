6f6b6da11f5257b16a92e5ceda4c06d42c3bc474
vanyauhalin <vanyauhalin@gmail.com>
2022-05-17 22:57:22 +0300

    refactor(base): move eslint rules to extends

--- packages/eslint-config-base/lib/index.js
+++ packages/eslint-config-base/lib/index.js
@@ -1,4 +1,3 @@
-const eslint = require('./eslint');
 const eslintPluginImport = require('./eslint-plugin-import');
 const eslintPluginPromise = require('./eslint-plugin-promise');
 const eslintPluginUnicorn = require('./eslint-plugin-unicorn');
@@ -7,37 +6,21 @@ const eslintPluginUnicorn = require('./eslint-plugin-unicorn');
  * @type {import('eslint').Linter.BaseConfig}
  */
 module.exports = {
-  env: {
-    browser: true,
-    es2022: true,
-    node: true,
-  },
   extends: [
     'airbnb-base',
     'plugin:unicorn/recommended',
     ...[
+      './eslint',
       './eslint-plugin-jsonc',
     ].map((config) => require.resolve(config)),
   ],
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-  },
   plugins: [
     'promise',
     'unicorn',
   ],
   rules: {
-    ...eslint,
     ...eslintPluginImport,
     ...eslintPluginPromise,
     ...eslintPluginUnicorn,
   },
-  ignorePatterns: [
-    '!.*',
-    '.local',
-    'dist',
-    'lib',
-    'node_modules',
-  ],
 };
