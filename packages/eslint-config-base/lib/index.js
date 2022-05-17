f692381565ea9295c087e945e991311aef44f45a
vanyauhalin <vanyauhalin@gmail.com>
2022-05-17 23:06:14 +0300

    refactor(base): move unicorn rules to extends

--- packages/eslint-config-base/lib/index.js
+++ packages/eslint-config-base/lib/index.js
@@ -1,23 +1,15 @@
-const eslintPluginUnicorn = require('./eslint-plugin-unicorn');
-
 /**
  * @type {import('eslint').Linter.BaseConfig}
  */
 module.exports = {
   extends: [
     'airbnb-base',
-    'plugin:unicorn/recommended',
     ...[
       './eslint',
       './eslint-plugin-import',
       './eslint-plugin-jsonc',
       './eslint-plugin-promise',
+      './eslint-plugin-unicorn',
     ].map((config) => require.resolve(config)),
   ],
-  plugins: [
-    'unicorn',
-  ],
-  rules: {
-    ...eslintPluginUnicorn,
-  },
 };
