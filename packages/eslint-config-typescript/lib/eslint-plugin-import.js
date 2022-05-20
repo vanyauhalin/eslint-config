7943b8a98f7b35aa1bb1cd5a1c816c118d83dab3
vanyauhalin <vanyauhalin@gmail.com>
2022-05-20 13:02:43 +0300

    fix(typescript): sort overrides

--- packages/eslint-config-typescript/lib/eslint-plugin-import.js
+++ packages/eslint-config-typescript/lib/eslint-plugin-import.js
@@ -6,16 +6,23 @@ const imp = require('@vanyauhalin/eslint-config-base/lib/eslint-plugin-import');
  * @see https://github.com/import-js/eslint-plugin-import
  */
 module.exports = {
-  rules: {
-    /**
-     * Forbid the import of external modules that are not declared
-     * in the `package.json`'s `dependencies`, `devDependencies`,
-     * `optionalDependencies`, `peerDependencies`, or `bundledDependencies`.
-     */
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: imp.rules['import/no-extraneous-dependencies'][1]
-        .devDependencies
-        .map((file) => file.replace(path.extname(file), '.{js,ts}')),
-    }],
-  },
+  overrides: [
+    {
+      files: '*.ts',
+      rules: {
+        /**
+         * Forbid the import of external modules that are not declared
+         * in the `package.json`'s `dependencies`, `devDependencies`,
+         * `optionalDependencies`, `peerDependencies`, or `bundledDependencies`.
+         * Override `eslint-config-base/lib/eslint-plugin-import.js`.
+         * @see https://github.com/import-js/eslint-plugin-import/blob/376747914b47fbdcf99212b9e9bd4d5e09825385/docs/rules/no-extraneous-dependencies.md
+         */
+        'import/no-extraneous-dependencies': ['error', {
+          devDependencies: imp.rules['import/no-extraneous-dependencies'][1]
+            .devDependencies
+            .map((file) => file.replace(path.extname(file), '.ts')),
+        }],
+      },
+    },
+  ],
 };
