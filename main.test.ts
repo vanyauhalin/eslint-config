2bd2dc94c20f86c007fbae114064d183c52b0a68
vanyauhalin <vanyauhalin@gmail.com>
2024-12-31 23:26:01 +0400

    do not use a newer node test runner api to be able to run on older versions of node

--- main.test.ts
+++ main.test.ts
@@ -1,13 +1,34 @@
 import path from "node:path"
-import {after, suite, test} from "node:test"
+import {test} from "node:test"
 import {RuleTester} from "@typescript-eslint/rule-tester"
 
-/* eslint-disable typescript/no-misused-promises */
-RuleTester.afterAll = after
-RuleTester.describe = suite
-RuleTester.it = test
-RuleTester.itOnly = test.only
-/* eslint-enable typescript/no-misused-promises */
+// The code listed below might appear weird, but:
+//
+// 1. The Node.js native test runner was chosen for its simplicity and the
+// ability to run all tests from a single entry point without external
+// command-line interfaces.
+//
+// 2. However, the part of the API of the native Node.js test runner was
+// introduced only in version 20, which conflicts with the idea that the project
+// needed to be tested on older Node.js versions.
+
+RuleTester.afterAll = function afterAll(cb) {
+	test.after(cb)
+}
+
+RuleTester.describe = function describe(_, cb) {
+	cb()
+}
+
+RuleTester.it = function it(n, cb) {
+	// eslint-disable-next-line no-void
+	void test(n, cb)
+}
+
+RuleTester.itOnly = function itOnly(n, cb) {
+	// eslint-disable-next-line no-void
+	void test(n, {only: true}, cb)
+}
 
 let a = process.argv.splice(2)
 let d = process.cwd()
