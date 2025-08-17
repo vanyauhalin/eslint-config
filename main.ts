8357952bd71a33b9dadf4515e62fece36297b120
vanyauhalin <hi@vanyauhalin.me>
2025-08-17 15:27:50 +0400

    enable package-json/require-engines

--- main.ts
+++ main.ts
@@ -953,6 +953,7 @@ const config: TSESLint.FlatConfig.ConfigArray = [
 			"package-json/require-author": [e, {ignorePrivate: true}],
 			"package-json/require-bugs": [e, {ignorePrivate: true}],
 			"package-json/require-description": [e, {ignorePrivate: true}],
+			"package-json/require-engines": [e, {ignorePrivate: true}],
 			"package-json/require-keywords": [e, {ignorePrivate: true}],
 			"package-json/require-name": e,
 			"package-json/require-type": e,
